1c8583de4786118cefd3fadd4183ebfe70dc9e8f
Unknown <s_d_a@mail.ru>
2018-01-29 18:17:22 +0300

    Second commit.

--- src/J3Lesson1/Test.java
+++ src/J3Lesson1/Test.java
@@ -1,7 +1,16 @@
 package J3Lesson1;
 
+import java.util.Random;
+
+
 public class Test {
     public static void main(String[] args) {
+        Random random = new Random();
         System.out.println("Bla-bla!");
+        int m;
+        for (int i = 0; i < 5; i++){
+            m = random.nextInt(100);
+            System.out.println(m);
+        }
     }
 }
