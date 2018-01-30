35df1d9a903ce48c876d1cc659c853c0e69ec2d1
SD <trroublestarter@gmail.com>
2018-01-30 22:44:16 +0300

    Commit

--- src/J3Lesson1/GenTest.java
+++ src/J3Lesson1/GenTest.java
@@ -3,11 +3,20 @@ package J3Lesson1;
 public class GenTest<T> {
     T obj;
 
-    GenTest (T obj){
+    GenTest(T obj) {
         this.obj = obj;
     }
 
-    public T GetObj(){
+    public T GetObj() {
         return obj;
+    }
+
+    public String showType() {
+        return "Object is" + obj.getClass().getName();
+    }
+
+    public static void main(String[] args) {
+        GenTest<Integer> inTT = new GenTest<>(678);
+        System.out.println(inTT.showType() + " value:" + inTT.GetObj());
     }
 }
