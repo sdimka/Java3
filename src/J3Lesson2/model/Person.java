bcc463438580584573ce62953b09329c8c82194a
SD-from work <sd@work>
2018-02-08 21:58:21 +0300

    Complete HW2 part 1

---
+++ src/J3Lesson2/model/Person.java
@@ -0,0 +1,42 @@
+package J3Lesson2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+//@Table(name="person")
+public class Person {
+
+    @Id
+    //@Column(name="id")
+    int    id;
+    //@Column(name="name")
+    String name;
+
+    public Person() {}
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Person {id = " + String.valueOf(id) + ", name = '" + name + "'}";
+    }
+}
