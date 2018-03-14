c01796afa9aece060b7adb097284ff22125b477a
SD-from work <sd@work>
2018-03-14 19:39:34 +0300

    HW7, annotation test begin

--- src/J3Lesson7t/MyTest.java
+++ src/J3Lesson7t/MyTest.java
@@ -1,10 +1,21 @@
 package J3Lesson7t;
 
 
-
 public class MyTest {
 
-    static void start(Class<?> cls){
-
+    @Test(priority = 1)
+    public void test1() {
+        System.out.println("Running test # 1");
     }
+
+    @Test(priority = 2)
+    public void test2() {
+        System.out.println("Running test # 2");
+    }
+
+    @Test(priority = 3)
+    public void test3() {
+        System.out.println("Running test # 3");
+    }
+
 }
--- src/J3Lesson7t/Tester.java
+++ src/J3Lesson7t/Tester.java
@@ -1,11 +1,47 @@
 package J3Lesson7t;
 
+import java.lang.annotation.*;
 import java.lang.reflect.Method;
-import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 public class Tester {
 
+    static Class<?> clazz = MyTest.class;
+
+    public static void main(String[] args) {
+//        Method[] methods = clazz.getMethods();
+        Object mt = new MyTest();
+        String methodName = "test1";
+        try {
+            Method method = mt.getClass().getMethod(methodName);
+            method.invoke(mt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+//        for (Method m : methods) {
+//            Annotation[] annotation = m.getAnnotations();
+//            for (Annotation a: annotation){
+//                System.out.println(a.toString());
+//            }
+//            start(mt, m);
+//            System.out.println(m.toString());
+//        }
+
+    }
+
+    static void start(Class<?> cls, Method m){
+//        Class<?> cl = new cls;
+//        Method mth = cls.getMethod(m);
+//            m.invoke(cls);
+
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+@interface Test {
+    int priority();
 }
